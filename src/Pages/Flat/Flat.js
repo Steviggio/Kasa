3601a97685b057207efdd93692baf1f235f5cefc
Steve <steve.mothmora@gmail.com>
2023-06-24 18:39:28 +0200

    Home mobile display done

--- src/Pages/Flat/Flat.js
+++ src/Pages/Flat/Flat.js
@@ -5,11 +5,30 @@ import Dropdown from '../../components/Dropdown/Dropdown';
 import Carousel from '../../components/Carousel/Carousel';
 import Flats from '../../assets/data/data.json';
 import Tag from '../../components/Tag/Tag';
+import FullStar from "../../assets/img/full-star.png";
+import EmptyStar from "../../assets/img/empty-star.png";
 
 function Flat() {
 
   const id = useParams();
   const flat = Flats.find((flat) => (flat.id === id.id))
+  
+  function generateStars (rating) {
+    const fullStars = Math.floor(rating);
+    const emptyStars = 5 - fullStars;
+
+    const stars = [];
+
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(<img key={i} src={FullStar} alt='Full Star'/>);
+    }
+
+    for (let i= 0; i < emptyStars; i++) {
+      stars.push(<img key={i} src={EmptyStar} alt='Empty Star' />)
+    }
+
+    return stars;
+  }
 
   const flatTag = flat?.tags.map((tags, index) => {
     return <Tag key={index} tag={tags} />
@@ -18,18 +37,20 @@ function Flat() {
   const equipments = flat?.equipments.map((equipment, index) =>{
   return <li key={index}>{equipment}</li>})
 
+
+
   return (
     <>
       <div className='carousel-flat-container'>
         <Carousel pictures={flat.pictures} />
-        <div>
+        <div className='flat-host-container'>
           <div className='flat-informations'>
             <h2 className='flat-informations-title'>{flat.title}</h2>
             <span className='flat-informations-location'>{flat.location}</span>
           </div>
           <div className='host-informations'>
-            <span>Nom de l'hote</span>
-            {/* <img alt='Host portrait'>image de l'hôte</img> */}
+            <span>{flat.host.name}</span>
+            <img alt='Host portrait' src={flat.host.picture}/>
           </div>
         </div>
         <div className='tag-stars-container'>
@@ -37,7 +58,7 @@ function Flat() {
             {/* Itération sur les tag présents dans la liste associé à  l'appartement */}
             {flatTag}
           </div>
-          <div className='stars-container'>Stars</div>
+          <div className='stars-container'>{generateStars(Number(flat.rating))}</div>
         </div>
         <div className='flat-dropdown-container'>
         <Dropdown className='dropdown-bars' title='Description' description={flat.description} />
