b00edf3696816dc11b933d7264a1a6b321c5f51c
Steve <steve.mothmora@gmail.com>
2023-06-25 21:38:36 +0200

    Ending the mobile display for every pages

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -7,9 +7,9 @@ import './About.scss'
 function About() {
   return (
     <div className="about">
-      <Banner cover={aboutImage} text='' />
+      <Banner className='about-banner' cover={aboutImage} text='' />
       <div className="about-container">
-        <Dropdown title={"Fiabilité"} description={"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes"} />
+        <Dropdown title={"Fiabilité"} description={"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."} />
         <Dropdown title={"Respect"} description={"La bienvaillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."}/>
         <Dropdown title={"Service"} description={"La bienvaillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."}/>
         <Dropdown title={"Sécurité"} description={"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestiques pour nos hôtes."}/>
--- src/Pages/Flat/Flat.js
+++ src/Pages/Flat/Flat.js
@@ -12,18 +12,18 @@ function Flat() {
 
   const id = useParams();
   const flat = Flats.find((flat) => (flat.id === id.id))
-  
-  function generateStars (rating) {
+
+  function generateStars(rating) {
     const fullStars = Math.floor(rating);
     const emptyStars = 5 - fullStars;
 
     const stars = [];
 
     for (let i = 0; i < fullStars; i++) {
-      stars.push(<img key={i} src={FullStar} alt='Full Star'/>);
+      stars.push(<img key={i} src={FullStar} alt='Full Star' />);
     }
 
-    for (let i= 0; i < emptyStars; i++) {
+    for (let i = 0; i < emptyStars; i++) {
       stars.push(<img key={i} src={EmptyStar} alt='Empty Star' />)
     }
 
@@ -34,8 +34,9 @@ function Flat() {
     return <Tag key={index} tag={tags} />
   })
 
-  const equipments = flat?.equipments.map((equipment, index) =>{
-  return <li key={index}>{equipment}</li>})
+  const equipments = flat?.equipments.map((equipment, index) => {
+    return <li key={index}>{equipment}</li>
+  })
 
 
 
@@ -47,22 +48,26 @@ function Flat() {
           <div className='flat-informations'>
             <h2 className='flat-informations-title'>{flat.title}</h2>
             <span className='flat-informations-location'>{flat.location}</span>
+            <div className='flat-tag-container'>
+              {/* Itération sur les tag présents dans la liste associé à  l'appartement */}
+              {flatTag}
+            </div>
           </div>
-          <div className='host-informations'>
-            <span>{flat.host.name}</span>
-            <img alt='Host portrait' src={flat.host.picture}/>
+          <div className='host-stars-informations'>
+            <div className='host-informations'>
+              <span>{flat.host.name}</span>
+              <img alt='Host portrait' src={flat.host.picture} />
+            </div>
+            <div className='stars-container'>{generateStars(Number(flat.rating))}</div>
           </div>
-        </div>
-        <div className='tag-stars-container'>
-          <div className='flat-tag-container'>
-            {/* Itération sur les tag présents dans la liste associé à  l'appartement */}
-            {flatTag}
-          </div>
-          <div className='stars-container'>{generateStars(Number(flat.rating))}</div>
+          {/* </div> */}
+          {/* <div className='tag-stars-container'> */}
+
+
         </div>
         <div className='flat-dropdown-container'>
-        <Dropdown className='dropdown-bars' title='Description' description={flat.description} />
-        <Dropdown className='dropdown-bars' title='Équipements' description={equipments} />
+          <Dropdown className='dropdown-bars left-up' title='Description' description={flat.description} />
+          <Dropdown className='dropdown-bars right-bottom' title='Équipements' description={equipments} />
         </div>
       </div>
 
